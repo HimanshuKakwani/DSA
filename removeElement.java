127a2f9f2c3cb50268d76218f98a66f9ce029d1a
Himanshu Kakwani <92535601+HimanshuKakwani@users.noreply.github.com>
2025-07-21 00:55:20 +0530

    remove Element Leetcode

---
+++ removeElement.java
@@ -0,0 +1,19 @@
+package DSA;
+
+class Solution {
+
+;
+
+    public int removeElement(int[] nums, int val) {
+
+        int i =0;
+
+        for(int j = 0; j<nums.length;j++){
+            if(nums[j]!=val){
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+}
